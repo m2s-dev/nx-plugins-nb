936ef1a80dd0478fa1e8b4d7c8608b8938e00d80
Martin Hlavacek <martin@martinhlavacek.cz>
2020-07-31 22:06:00 +0200

    add new plugin and vscode

---
+++ packages/nx-plugin-boule/src/schematics/ngrx-new-action/rules/add-ngrx-to-package-json.ts
@@ -0,0 +1,18 @@
+import { Rule } from '@angular-devkit/schematics';
+import { addDepsToPackageJson } from '@nrwl/workspace';
+import { ngrxVersion } from '@nrwl/angular/src/utils/versions';
+
+export function addNgRxToPackageJson(): Rule {
+  return addDepsToPackageJson(
+    {
+      '@ngrx/store': ngrxVersion,
+      '@ngrx/effects': ngrxVersion,
+      '@ngrx/entity': ngrxVersion,
+      '@ngrx/router-store': ngrxVersion,
+    },
+    {
+      '@ngrx/schematics': ngrxVersion,
+      '@ngrx/store-devtools': ngrxVersion,
+    }
+  );
+}
